8b60b4c4cf26629a5395baef4e1d2bbd801adfab
Mayank Gupta <ubuntu568@gmail.com>
2021-03-20 16:16:45 +0530

    Get Queries in a Database
    External .py script to get the queries in a Database.

---
+++ tables.py
@@ -0,0 +1,8 @@
+def tables_in_sqlite_db(conn):
+    cursor = conn.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [
+        v[0] for v in cursor.fetchall()
+        if v[0] != "sqlite_sequence"
+    ]
+    cursor.close()
+    return tables
