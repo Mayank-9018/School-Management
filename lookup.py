ee330d484502b2f7113731f714facb165bba4df1
Mayank Gupta <ubuntu568@gmail.com>
2021-03-20 16:12:17 +0530

    Add proper handling with different type of data
    Make changes so that it can work with any database, with any queries. Add implementation of searching through all queries of the database.

--- lookup.py
+++ lookup.py
@@ -1,8 +1,24 @@
 import sqlite3
+from tkinter.messagebox import showerror
+
 con = sqlite3.connect('data.db')
 cur = con.cursor()
 
-def search(field,value):
-    new_val = '%'+value+'%'
-    h = cur.execute(f"SELECT * from Students WHERE {field} LIKE ? UNION ALL SELECT * from Teachers WHERE {field} LIKE ?",(new_val,new_val))
-    return h
+def search(table,field,value,tables):
+    if not value.strip()=='':
+        new_val = '%'+value+'%'
+    else:
+        new_val = '%'
+    if not field=='':
+        if not table=='All':
+            sql = f"SELECT * from {table} WHERE {field} LIKE '{new_val}'"
+        else:
+            select_sql = tuple(f"Select * from {t} WHERE {field} LIKE '{new_val}'" for t in tables[1:])
+            sql = ' UNION ALL '.join(select_sql)
+        h = cur.execute(sql)
+        return h
+    else:
+        showerror(title='Field Error',message='Field Cannot be empty.')
+        return ()
+
+search('Workers','Salary','',['All','Students','Teachers','Workers'])
